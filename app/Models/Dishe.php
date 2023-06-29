12f27268ffb6f3d7a9a3698da8c906853bf04fab
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-06-29 17:00:36 +0200

    add foreign to dishes & link model dishe and restaurant

--- app/Models/Dishe.php
+++ app/Models/Dishe.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Dishe extends Model
 {
     use HasFactory;
+
+    public function type() {
+        return $this->belongsTo(Restaurant::class);
+    }
 }
