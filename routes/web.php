053eba52cb3af1d1438fe986842191646e41fe69
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-06-30 16:27:12 +0200

    add restaurant resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\RestaurantController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('restaurants', RestaurantController::class );
 });
 
 require __DIR__.'/auth.php';
