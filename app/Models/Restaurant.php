84ac3567380051043fc34cb6866abac784fde941
silvanoguel <silvanoguel@msn.com>
2023-06-30 10:47:01 +0200

    add:type migration and model

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -9,7 +9,13 @@ class Restaurant extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'address', 'phone', 'image', 'vat_number'];
+
     public function dishes() {
         return $this->hasMany(Dishe::class);
+    }
+
+    public function types() {
+        return $this->belongsToMany(Type::class);
     }
 }
