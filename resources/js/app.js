d981bd37fe1d909ce1e5db777af0ecb3f700c443
FilippoFoti <filippofoti7@gmail.com>
2023-07-03 15:48:09 +0200

    Delete confirm & edit button

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,28 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+
+const deleteBtns = document.querySelectorAll(".btn-delete");
+
+if (deleteBtns.length > 0) {
+    deleteBtns.forEach((btn) => {
+        btn.addEventListener("click", function (event) {
+            event.preventDefault();
+            const restaurantTitle = btn.getAttribute("data-restaurant-title");
+
+            const deleteModal = new bootstrap.Modal(
+                document.getElementById("delete-modal")
+            );
+            document.getElementById("restaurant-title").innerText = restaurantTitle;
+
+            document
+                .getElementById("action-delete")
+                .addEventListener("click", function () {
+                    btn.parentElement.submit();
+                });
+
+            deleteModal.show();
+        });
+    });
+}
