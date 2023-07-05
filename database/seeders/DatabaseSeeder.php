6b8a9fbcd163f856a3fa93bd827dd11af644c9e0
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-07-05 12:26:34 +0200

    Seeder commentati

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            RestaurantTableSeeder::class,
-            DisheTableSeeder::class,
+            // RestaurantTableSeeder::class,
+            // DisheTableSeeder::class,
             TypeTableSeeder::class
         ]);
     }
