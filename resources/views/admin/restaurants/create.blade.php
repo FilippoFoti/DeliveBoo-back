b1e2c6a4fb9d7391f0f7e24df43cd6dcac9b746b
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-07-06 11:03:12 +0200

    Fix: checkbox input

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -64,8 +64,7 @@
                         <p>Seleziona il tipo</p>
                         @foreach ($types as $type)
                             <div class="form-check">
-                                <input class="form-check-input" name="type_id[]" type="checkbox"
-                                    value="{{ $type->id }}" id="type-{{ $type->id }}">
+                                <input class="form-check-input @error('type_id') is-invalid @enderror" name="type_id[]" type="checkbox" value="{{ $type->id }}" id="type-{{ $type->id }}" @checked(in_array($type->id, old('type_id', [])))>
                                 <label class="form-check-label" for="type-{{ $type->id }}">
                                     {{ $type->name }}
                                 </label>
