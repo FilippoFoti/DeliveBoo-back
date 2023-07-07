0068b6bdf85bd459d371d336f6a62fcc4a5f70bd
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-07-07 16:57:58 +0200

    fixed api/restaurantController

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -12,16 +12,19 @@ use function PHPSTORM_META\type;
 
 class RestaurantController extends Controller
 {
-    public function index(Request $request) {
-        if($request->has('type_id')){
-            $restaurants = Restaurant::with(['type'])->where('type_id', $request->type_id)->paginate(20);
-        } else {
-            $restaurants = Restaurant::with('types','dishes')->get();
+    public function index(Request $request)
+    {
+        $query = Restaurant::with(['types']);
+        if ($request->has('type_id')) {
+            $query->whereHas('types', function ($q) use ($request) {
+                $q->whereIn('id', $request->type_id);
+            });
         }
+        $restaurants = $query->get();
         // $restaurants = Restaurant::with('types','dishes')->get();
         return response()->json([
             'success' => true,
-            'restaurants' => $restaurants
+            'results' => $restaurants
         ]);
     }
 }
