51de01270c19116ea258ec94195b7b733788c516
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-07-11 17:09:28 +0200

    Edit: payment controller

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dishe;
+use App\Models\Order;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
 
@@ -21,25 +23,49 @@ class PaymentController extends Controller
 
     public function makePayment(Request $request, Gateway $gateway)
     {
+        $data = $request->all();
+        $amount = 0;
+
+        $cart = $data['cart'];
+
+        foreach ($cart as $item) {
+            $dishe = Dishe::where('id', $item['id'])->first();
+            $amount += $dishe->price * $item['count'];
+        }
+
         $result = $gateway->transaction()->sale([
-            'amount' => $request->amount,
+            'amount' => $amount,
             'paymentMethodNonce' => $request->token,
             'options' => [
                 'submitForSettlement' => true
             ],
         ]);
+
         if ($result->success) {
-            $data = [
-                'message' => 'Transazione approvata',
-                'success' => true
-            ];
-            return response()->json($data);
-        } else {
-            $data = [
-                'message' => 'Transazione rifiutata',
-                'success' => false
-            ];
-            return response()->json($data);
+            $order = new Order();
+            $data['total_price'] = $amount;
+            $order->fill($data);
+            $order->save();
         }
+        // $result = $gateway->transaction()->sale([
+        //     'amount' => $request->amount,
+        //     'paymentMethodNonce' => $request->token,
+        //     'options' => [
+        //         'submitForSettlement' => true
+        //     ],
+        // ]);
+        // if ($result->success) {
+        //     $data = [
+        //         'message' => 'Transazione approvata',
+        //         'success' => true
+        //     ];
+        //     return response()->json($data);
+        // } else {
+        //     $data = [
+        //         'message' => 'Transazione rifiutata',
+        //         'success' => false
+        //     ];
+        //     return response()->json($data);
+        // }
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,7 +9,18 @@ class Order extends Model
 {
     use HasFactory;
 
-    public function dishes() {
+    public function dishes()
+    {
         return $this->belongsToMany(Dishe::class);
     }
+    protected $fillable = [
+        'customer_name',
+        'customer_lastname',
+        'email',
+        'phone',
+        'address',
+        'date',
+        'total_price',
+        'payment_status',
+    ];
 }
