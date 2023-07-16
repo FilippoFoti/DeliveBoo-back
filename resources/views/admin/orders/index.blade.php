2ba85194ab76f43a977292f057afcf46eefef155
silvanoguel <silvanoguel@msn.com>
2023-07-16 23:37:12 +0200

    grafico ok

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,39 +1,103 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
-  <h1 class="pt-3 pb-1 text-center m-0">Riepilogo Ordini</h1>
-  <div class="text-end m-3">
-    <a href="{{ route('admin.dashboard') }}" class="btn btn-primary mb-2">Torna alla dashboard</a>
-    <div class="row row-cols-3">
-      <table class="table table-striped">
-        <thead>
-          <tr class="text-center">
-            <th scope="col">ID Ordine</th>
-            <th scope="col">Data</th>
-            <th scope="col">Nome Cliente</th>
-            <th scope="col">Cognome Cliente</th>
-            <th scope="col">Totale</th>
-            <th scope="col">Azioni</th>
-          </tr>
-        </thead>
-        <tbody>
+    <div class="container">
+        <h1 class="pt-3 pb-1 text-center m-0">Riepilogo Ordini</h1>
+        <div class="text-end m-3">
+            <a href="{{ route('admin.dashboard') }}" class="btn btn-primary mb-2">Torna alla dashboard</a>
+            <div class="row row-cols-3">
+                <table class="table table-striped">
+                    <thead>
+                        <tr class="text-center">
+                            <th scope="col">ID Ordine</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Nome Cliente</th>
+                            <th scope="col">Cognome Cliente</th>
+                            <th scope="col">Ammontare Vendite</th> <!-- Aggiungi questa colonna -->
+                            <th scope="col">Azioni</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($ordersDesc as $order)
+                            <tr class="text-center">
+                                <td class="align-middle">{{ $order->id }}</td>
+                                <td class="align-middle">{{ \Carbon\Carbon::parse($order->date)->format('d-m-Y') }}</td>
+                                <td class="align-middle">{{ $order->customer_name }}</td>
+                                <td class="align-middle">{{ $order->customer_lastname }}</td>
+                                <td class="align-middle">{{ $order->total_price }} €</td>
+                                <td><a href="{{ route('admin.orders.show', $order->id) }}" class="btn btn-success">
+                                        <i class="fa-solid fa-eye"></i>
+                                    </a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+            </div>
 
-          @foreach ($ordersDesc as $order)
-          <tr class="text-center">
-            <td class="align-middle">{{ $order->id }}</td>
-            <td class="align-middle">{{ \Carbon\Carbon::parse($order->date)->format('d-m-Y') }}</td>
-            <td class="align-middle">{{ $order->customer_name }}</td>
-            <td class="align-middle">{{ $order->customer_lastname }}</td>
-            <td class="align-middle">{{ $order->total_price }} €</td>
-            <td><a href="{{ route('admin.orders.show', $order->id) }}" class="btn btn-success">
-                <i class="fa-solid fa-eye"></i>
-              </a></td>
+        </div>
 
-          </tr>
-          @endforeach
+        <div>
+            <canvas id="myChart"></canvas>
+        </div>
 
-        </tbody>
-    </div>
-  </div>
-  @endsection
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+        <body>
+            <canvas id="myChart"></canvas>
+
+            <script>
+                <?php
+                $monthlyOrders = [];
+                foreach ($ordersDesc as $order) {
+                    $monthYear = date('F Y', strtotime($order->date)); // Ottieni il mese e l'anno dell'ordine
+                    if (!isset($monthlyOrders[$monthYear])) {
+                        $monthlyOrders[$monthYear] = 0;
+                    }
+                    $monthlyOrders[$monthYear]++;
+                }
+                
+                $labels = array_keys($monthlyOrders);
+                $data = array_values($monthlyOrders);
+                ?>
+
+                // Dati di esempio per la tabella
+                var data = {
+                    labels: <?php echo json_encode($labels); ?>,
+                    datasets: [{
+                        label: "Ordini",
+                        data: <?php echo json_encode($data); ?>,
+                        backgroundColor: "#FFEB3B",
+                        borderColor: "#FFEB3B",
+                        borderWidth: 1
+                    }]
+                };
+
+                // Opzioni per il grafico
+                var options = {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            precision: 0,
+                            stepSize: 1,
+                            ticks: {
+                                callback: function(value) {
+                                    if (Number.isInteger(value)) {
+                                        return value.toString();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+
+                // Creazione del grafico a barre
+                var ctx = document.getElementById("myChart").getContext("2d");
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: data,
+                    options: options
+                });
+            </script>
+        </body>
+
+        </html>
+    @endsection
